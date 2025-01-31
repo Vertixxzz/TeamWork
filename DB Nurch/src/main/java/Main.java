3fabaab918bca07b29233688024de63dcff63273
Nurdauletamangos <nurdauletamangosvps@gmail.com>
2025-02-01 04:27:35 +0500

    Start

---
+++ DB Nurch/src/main/java/Main.java
@@ -0,0 +1,9 @@
+import controllers.DishController;
+
+public class Main {
+    public static void main(String[] args) {
+        DishController dishController = new DishController();
+        System.out.println("Welcome to our Restaurant Menu:");
+        dishController.displayMenu();
+    }
+}
